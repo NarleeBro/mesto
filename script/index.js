d06614892d12b539a8871ec7b8110e85d882c238
Lenar Nasibullin <linarpr@yandex.ru>
2023-03-06 21:07:01 +0300

    feat написан js код для submit и value, подкорректирована форма popup

--- script/index.js
+++ script/index.js
@@ -1,40 +1,40 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.popup__edit-button-open');
-console.log(popupOpenButtonElement);
+
+const popupFormElement = document.querySelector('.popup__form');
+const nameInputElement = popupFormElement.querySelector('.popup__input-name');
+const jobInputElement = popupFormElement.querySelector('.popup__input-job');
+
+const profileNameTitle = document.querySelector('.profile__title');
+const profileJobSubtitle = document.querySelector('.profile__subtitle');
 
 const openPopup = function () {
     popupElement.classList.add('popup_is-opened');
-    console.log('Open popup clicked')
+    nameInputElement.value = profileNameTitle.textContent;
+    jobInputElement.value = profileJobSubtitle.textContent;
 }
 
 const closePopup = function () {
     popupElement.classList.remove('popup_is-opened');
 }
 
-const ClosePopupByClickOnOverlay = function (event) {
-console.log(event.target, event.currentTarget);
+const closePopupByClickOnOverlay = function (event) {
+/* console.log(event.target, event.currentTarget); */
 if (event.target !== event.currentTarget) {
     return;
 }
 closePopup();
 };
 
+const handleFormSubmit = function (evt) {
+    evt.preventDefault();
+    profileNameTitle.textContent = nameInputElement.value;
+    profileJobSubtitle.textContent = jobInputElement.value;
+    closePopup ();
+}
+
 popupOpenButtonElement.addEventListener('click', openPopup);
-/* popupOpenButtonElement.addEventListener('click', function (event) {
-  console.log(event);
-}); */
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', ClosePopupByClickOnOverlay);
-
-/* popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup); */
-
-/* const togglePopupVisibility = function () {
-    popupElement.classList.toggle('popup_is-opened');
-} */
-
-/* popupOpenButtonElement.addEventListener('click', togglePopupVisibility); */
-
-/* togglePopupVisibility(); */
-
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+popupFormElement.addEventListener('submit', handleFormSubmit);
