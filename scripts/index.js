f4df333eb2bc3bb1dc912c64e0d660807134a752
NarleeBro <linarpr@gmail.com>
2023-03-23 22:02:34 +0300

    fix js universal function and listener

--- scripts/index.js
+++ scripts/index.js
@@ -1,73 +1,17 @@
-// Ищем необходимые элементы на странице по классам (можно по другим ижентификаторам)
+// Ищем необходимые элементы на странице по классам (можно по другим идентификаторам)
 /* const popupElement = document.querySelector('.popup'); */
 const popupEditProfile = document.querySelector('.popup_section_edit-profile')
-const popupCloseButtonElement = document.querySelector('.popup__close');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupCloseButtonElement = document.querySelector('.popup__close'); ////////убрать????????
+const popupCloseButtonElements = document.querySelectorAll('.popup__close');
 
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 /* const popupFormElement = document.querySelector('.popup__form'); */ //форма родитель, в ней ищем классы с модификаторами
 const popupFormEditProfile = popupEditProfile.querySelector('.popup__form_edit_profile');
 const popupOpenButtonAddElement = document.querySelector('.profile__add-button');
 const nameInputElement = popupFormEditProfile.querySelector('.popup__input_edit_name');
 const jobInputElement = popupFormEditProfile.querySelector('.popup__input_edit_job');
-
 const profileNameTitle = document.querySelector('.profile__title');
 const profileJobSubtitle = document.querySelector('.profile__subtitle');
-
-
-//Функция которая передает введенные значения в попапе на страницу при ее закрытии 
-const handleFormEditProfileSubmit = function (evt) {
-    evt.preventDefault();
-    // Тут мы прописали, чтобы значение текста, которое мы напишем в открывшемся попапе, где имя "Кусто", передавалось при закрытии попапа на страницу
-    profileNameTitle.textContent = nameInputElement.value;
-    // Тут мы прописали, чтобы значение текста, которое мы напишем в открывшемся попапе, где должность "Исследоватеот океана", передавалось при закрытии попапа на стараницу    
-    profileJobSubtitle.textContent = jobInputElement.value;
-    closePopup ();
-};
-
-popupFormEditProfile.addEventListener('submit', handleFormEditProfileSubmit);
-
-// Функция, которая дает возможность закрыть попап в любом месте экрана, кроме самой формы попап */
-/* const closePopupByClickOnOverlay = function (event) {
-console.log(event.target, event.currentTarget);
-if (event.target !== event.currentTarget) {
-    return;
-}
-closePopup();
-}; */ 
-
-// Слушатель, который дает возможность закрыть попап в любом месте экрана, кроме самой формы попап (применяется с функцией закрытия попап)*/
-/* popupEditProfile.addEventListener('click', closePopupByClickOnOverlay); */
-
-/* ---------------------------------------------------------------------------------------------------- */
-// Мыссив для клонирования фотографий в template
-/* const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; */
-
-// Ищем необходимые элементы на странице по классам (можно по другим ижентификаторам)
 //Шаблон картинка + напдись + лайк + корзина
 const userCardsContainer = document.querySelector('.elements');
 const userTemplate = document.querySelector('.template').content;
@@ -88,6 +32,31 @@ const popupFormCreateCard = popupCreateCard.querySelector('.popup__form_create_c
 const popupButtonCloseCreateCard = popupCreateCard.querySelector('.popup__close_create-card');
 const placeNameInputCreateCard = popupFormCreateCard.querySelector('.popup__input_edit_place-name');
 const imageUrlInputCreateCard = popupFormCreateCard.querySelector('.popup__input_edit_image-url');
+
+//Функция которая передает введенные значения в попапе на страницу при ее закрытии 
+const handleFormEditProfileSubmit = function (evt) {
+    evt.preventDefault();
+    // Тут мы прописали, чтобы значение текста, которое мы напишем в открывшемся попапе, где имя "Кусто", передавалось при закрытии попапа на страницу
+    profileNameTitle.textContent = nameInputElement.value;
+    // Тут мы прописали, чтобы значение текста, которое мы напишем в открывшемся попапе, где должность "Исследоватеот океана", передавалось при закрытии попапа на стараницу    
+    profileJobSubtitle.textContent = jobInputElement.value;
+    closePopup (popupEditProfile);
+};
+
+popupFormEditProfile.addEventListener('submit', handleFormEditProfileSubmit);
+
+// Функция, которая дает возможность закрыть попап в любом месте экрана, кроме самой формы попап */
+/* const closePopupByClickOnOverlay = function (event) {
+console.log(event.target, event.currentTarget);
+if (event.target !== event.currentTarget) {
+    return;
+}
+closePopup();
+}; */ 
+
+// Слушатель, который дает возможность закрыть попап в любом месте экрана, кроме самой формы попап (применяется с функцией закрытия попап)*/
+/* popupEditProfile.addEventListener('click', closePopupByClickOnOverlay); */
+
 //создаем клон массива темплейт в новую переменную
 const createCard = function (item) {
   const cardElement = userTemplate.cloneNode(true);
@@ -103,7 +72,7 @@ const createCard = function (item) {
     popupImageElement.src = item.link;
     popupImageElement.alt = item.name;
     popupFigcaptionText.textContent = item.name;
-    openPopupLargeImage(popupIncreaseImage)
+    openPopup(popupIncreaseImage) //11111 слушатель для открытия попап увеличения размера картинки
   })
 
   setEventListeners(cardElement);
@@ -145,28 +114,13 @@ const handleNewCard = function (event) {
   event.target.reset();
   const placeInPage = createCard(placeNameImageURLElement);
   templateList.prepend(placeInPage);
-  closePopupCreateCard(popupCreateCard); 
+  /* closePopupCreateCard(popupCreateCard);  */
+  closePopup(popupCreateCard);
  };
 //вешаем слушатель на кнопку создать с функией создания новой карточки
 popupFormCreateCard.addEventListener('submit', handleNewCard);
 
-///////////!!!!!!!
-
-/* function openPopup (popup) {
-  popup.classList.add('popup_opened');
-}
-
-function closePopup (popup) {
-  popup.classList.remove('popup_opened');
-}
-
-popupOpenButtonElement.addEventListener('click', function () {
-  nameInputElement.value = profileNameTitle.textContent;
-  jobInputElement.value = profileJobSubtitle.textContent;
-  openPopup(popupEditProfile)
-}); */
-
-//111111111111 Функция которая добавляет класс к элементу, который мы нашли ранее через querySelector
+/* //11111 Функция которая открывает попап редакторования профиля №1
 const openPopup = function () {
   popupEditProfile.classList.add('popup_opened');
     // Тут мы прописали, чтобы значение текста, которое написано на странице "Профиль", где имя "Кусто", передавалось в открывающийся попап
@@ -175,34 +129,71 @@ const openPopup = function () {
     jobInputElement.value = profileJobSubtitle.textContent;
 };
 
-//111111111111 Функция которая удаляет класс у элемента, который мы нашли ранее через querySelector. Далее мы эти функции будем использовать для открытия и закрытия попап
+//11111 Функция которая закрывает попап редакторования профиля №1
 const closePopup = function () {
   popupEditProfile.classList.remove('popup_opened');
 };
 
+//11111 слушатели для открытия/закрытия попап редакторования профиля №1
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
-//1111111111111 Функция которая добавляет класс к элементу, который мы нашли ранее через querySelector (для попап создания новой карточки)
+//11111 Функция которая открывает попап создания новой карточки №2
 const openPopupCreateCard = function () {
   popupCreateCard.classList.add('popup_opened');
 };
-//11111111111 Функция которая удаляет класс у элемента, который мы нашли ранее через querySelector. Далее мы эти функции будем использовать для открытия и закрытия попапю (для попап создания новой карточки)ю
+//11111 Функция которая закрывает попап создания новой карточки №2
 const closePopupCreateCard = function () {
   popupCreateCard.classList.remove('popup_opened');
 };
-//1111111111111111111 слушатель для открытия попап создания новой карчтоки
+//11111 слушатели для открытия/закрытия попап создания новой карчтоки №2
 popupOpenButtonAddElement.addEventListener('click', openPopupCreateCard);
 popupButtonCloseCreateCard.addEventListener('click', closePopupCreateCard);
 
-//111111111111попап открытия карточки с увеличением размера
+//11111 Функция которая открывает попап карточки с увеличением размера №3
 const openPopupLargeImage = function () {
   popupIncreaseImage.classList.add('popup_opened');
 };
 
-//111111111111попап закрытия карточки с увеличением размера
+//11111 Функция которая закрывает попап карточки с увеличением размера №3
 const closePopupLargeImage = function () {
   popupIncreaseImage.classList.remove('popup_opened');
 };
-//слушатель для закрытия попап увеличения размера картинки
-popupButtonCloseIncreseImage.addEventListener('click', closePopupLargeImage);
+//11111 слушатель для закрытия попап увеличения размера картинки
+popupButtonCloseIncreseImage.addEventListener('click', closePopupLargeImage); */
+
+///////////!!!!!!!
+
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup (popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//функция открытия попап ред-я профиля с передачей значений со страницы в попап
+popupOpenButtonElement.addEventListener('click', function () {
+  nameInputElement.value = profileNameTitle.textContent;
+  jobInputElement.value = profileJobSubtitle.textContent;
+  openPopup(popupEditProfile)
+});
+
+//функция которая открывает попап создания новой карточки №2
+popupOpenButtonAddElement.addEventListener('click', function () {
+    openPopup(popupCreateCard)
+});
+
+//функция которая открывает попап карточки с увеличением размера №3
+popupImageElement.addEventListener('click', function () {
+  openPopup(popupIncreaseImage)
+});
+
+//функция которая закрывает попапы
+popupCloseButtonElements.forEach(function (element) {
+  element.addEventListener('click', function () {
+    closePopup(popupEditProfile);
+    closePopup(popupCreateCard);
+    closePopup(popupIncreaseImage);
+  })
+});
